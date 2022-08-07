502ab06fbb41a570bb1e2ddad45830b5933f5655
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-07 20:37:55 +0900

    renew

--- app/src/main/java/com/mnemosyne/webviewtest/SearchAll.java
+++ app/src/main/java/com/mnemosyne/webviewtest/SearchAll.java
@@ -1,26 +1,53 @@
 package com.mnemosyne.webviewtest;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.webkit.ConsoleMessage;
 import android.webkit.JavascriptInterface;
 import android.webkit.JsResult;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
 
 public class SearchAll extends AppCompatActivity {
     LinearLayout layout;
+    SharedPreferences spf;
+    String reserveUrl = "";
+    String urlHeader = "http://mnemosynesolutions.co.kr:8080/";
+    Integer callback_count = 0;
+    Hashtable<String, Hashtable<String, String>> htLogin;
+    ArrayList<String> callbackClubs = new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,25 +56,127 @@ public class SearchAll extends AppCompatActivity {
         // 상단 타이틀 변경
         setTitle("teezzim multi dynamic webview");
 
-        layout = findViewById(R.id.cover);
+        // preference
+        spf = getSharedPreferences("DEVICE", MODE_PRIVATE);
 
-        WebView webView = new WebView(getApplication());
-        WebSettings ws = webView.getSettings();
+        // 로그인 관리자 계정
+        String strAccountResult = getPostCall(urlHeader + "account", "{}");
+        setLoginAdminAccount(strAccountResult);
+
+        layout = findViewById(R.id.cover);
+        List<String> arrClubs = Arrays.asList(
+                "360cc", "acro", "adelscott", "adonis", "allday",
+                "alpensia", "alpsdy", "andonglake", "aramir", "ariji",
+                "arista", "arumdaun", "asecovalley", "baekje", "base",
+                "bavista", "baystars", "beache", "beaconhills", "bearsbest"
+        );
+        JSONObject prm = new JSONObject();
+        JSONArray arr = new JSONArray();
+        try {
+            for(int i = 0; i < arrClubs.size(); i++) {
+                arr.put(arrClubs.get(i));
+            }
+            prm.put("clubs", arr);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String param = prm.toString();
+        String strResult = getPostCall(urlHeader + "reserveSearchbots_admin", param);
+
+        JSONObject json;
+        JSONObject scripts;
+        JSONObject urls;
+        JSONObject UUIDs;
+        try {
+            json = new JSONObject(strResult);
+
+            scripts = new JSONObject(json.getString("scripts"));
+            urls = new JSONObject(json.getString("urls"));
+            UUIDs = new JSONObject(json.getString("ids"));
+
+            Log.d("clubs", json.getString("scripts"));
+            Log.d("clubs", json.getString("urls"));
+            Log.d("clubs", json.getString("ids"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        ArrayList<WebView> wvList = new ArrayList<WebView>();
+        for(int i = 0; i < arrClubs.size(); i++) {
+            Log.d("webview", i+"");
+            WebView wv = new WebView(getApplication());
+            String club= arrClubs.get(i);
+
+            // params into template script
+            String deviceId = spf.getString("UUID", "");
+            String deviceToken = spf.getString("token", "");
+            Hashtable<String, String> params = new Hashtable<String, String>();
+            Hashtable<String, String> idpw = htLogin.get(club);
+            Log.d("club", club + " : " + idpw.get("id") + " : " + idpw.get("pw"));
+            params.put("deviceId", deviceId);
+            params.put("deviceToken", deviceToken);
+            params.put("login_id", idpw.get("id"));
+            params.put("login_password", idpw.get("pw"));
+
+            try {
+                params.put("golfClubId", UUIDs.getString(club));
+                String script = setStringTemplate(params, scripts.getString(club));
+                setWebView(wv, club, script);
+                layout.addView(wv);
+                wv.loadUrl(urls.getString(club));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void setLoginAdminAccount(String strAccountResult) {
+        // json parse
+        JSONObject jsonAccount;
+        JSONObject jsonClubs;
+        htLogin = new Hashtable<String, Hashtable<String, String>>();
+        try {
+            jsonAccount = new JSONObject(strAccountResult);
+            Log.d("accounts", jsonAccount.getString("accounts"));
+            jsonClubs = new JSONObject(jsonAccount.getString("accounts"));
+
+            Iterator iter = jsonClubs.keys();
+            while(iter.hasNext()) {
+                String club = (String) iter.next();
+                JSONObject val = (JSONObject) jsonClubs.get(club);
+                String id = (String) val.get("id");
+                String pw = (String) val.get("pw");
+                Log.d("val", val.get("id") + "::" + val.get("pw"));
+                setIdPw(htLogin, club, id, pw);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
+    };
+    public void setWebView(WebView wv, String club, String script) {
+        Log.d("MSG", club);
+        WebSettings ws = wv.getSettings();
         ws.setJavaScriptEnabled(true);
         ws.setDomStorageEnabled(true);
         WebViewClient wvc = new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
             }
             @Override
             public void onPageFinished(WebView view, String url) {
-                Log.d("script", "search webview loaded!!");
-                // view.loadUrl(searchScript);
+                Log.d("jsLog", script + " :: " + club);
+                view.loadUrl(script);
                 super.onPageFinished(view, url);
             }
         };
-        webView.setWebChromeClient(new WebChromeClient() {
+        wv.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onConsoleMessage(ConsoleMessage message) {
                 try{
@@ -58,27 +187,28 @@ public class SearchAll extends AppCompatActivity {
             }
             @Override
             public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-                Log.d("jsLog", message);
+                Log.d("jsAlert", message + " :: " + club);
                 result.confirm();
                 return true;
             }
             @Override
             public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
-                Log.d("jsConfirm", message);
+                Log.d("jsConfirm", message + " :: " + club);
                 result.confirm();
                 return true;
             }
         });
-        AndroidController ac = new AndroidController(webView);
-        webView.addJavascriptInterface(ac, "AndroidController");
-
-        layout.addView(webView);
-    }
+        wv.setWebViewClient((wvc));
+        AndroidController ac = new AndroidController(wv, club);
+        wv.addJavascriptInterface(ac, "AndroidController");
+    };
     public class AndroidController {
         final public Handler handler = new Handler();
         private WebView WEBVIEW;
-        public AndroidController (WebView wv) {
+        private String CLUB;
+        public AndroidController (WebView wv, String club) {
             WEBVIEW = wv;
+            CLUB = club;
         };
         @JavascriptInterface
         public void message(final String message) {
@@ -86,10 +216,126 @@ public class SearchAll extends AppCompatActivity {
             handler.post(new Runnable() {
                 @Override
                 public void run() {
-                    WEBVIEW.loadUrl("javascript:(() => { console.log('end'); })();");
-                    // if(message.equals("end of procGolfSchedule!")) finish();
+                    if(message.equals("end of reserve/search")) {
+                        callback_count++;
+                        Log.d("callback", "normal: " + CLUB + " : " + callback_count);
+                        callbackClubs.add("normal : " + CLUB);
+                        for (int i = 0; i < callbackClubs.size(); i++) {
+                            Log.d("callback", callbackClubs.get(i));
+                        }
+                        layout.removeView(WEBVIEW);
+                    }
+                    if(message.equals("TZ_MSG_IC")) {
+                        callback_count++;
+                        Log.d("callback", "IC: " + CLUB + " : " + callback_count);
+                        callbackClubs.add("IC : " + CLUB);
+                        for (int i = 0; i < callbackClubs.size(); i++) {
+                            Log.d("callback", callbackClubs.get(i));
+                        }
+                        layout.removeView(WEBVIEW);
+                    }
                 }
             });
         };
     };
+    public String getPostCall(String url, String param) {
+        CallThread ct = new CallThread(url, param);
+        ct.start();
+        try {
+            ct.join();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        // http response 수신
+        String strResult = ct.getResult();
+        Log.d("RESULT", strResult);
+
+        return strResult;
+    };
+    public class CallThread extends Thread {
+        private String threadUrl;
+        private String threadParam;
+        private String Result;
+        public CallThread (String url, String param) {
+            threadUrl = url;
+            threadParam = param;
+        };
+        public void run() {
+            try{
+                URL testUrl = new URL(threadUrl);
+                HttpURLConnection urlConn = (HttpURLConnection) testUrl.openConnection();
+
+                // [2-1]. urlConn 설정.
+                urlConn.setConnectTimeout(15000);
+                urlConn.setReadTimeout(5000);
+                urlConn.setDoInput(true);
+                urlConn.setDoOutput(true);
+                urlConn.setUseCaches(false);
+                urlConn.setRequestMethod("POST"); // URL 요청에 대한 메소드 설정 : POST.
+                urlConn.setRequestProperty("Accept-Charset", "UTF-8"); // Accept-Charset 설정.
+                urlConn.setRequestProperty("Accept", "application/json");
+                // urlConn.setRequestProperty("Context_Type", "application/x-www-form-urlencode");
+                urlConn.setRequestProperty("Content-Type", "application/json");
+                // urlConn.setRequestProperty("apikey", ""); // ""안에 apikey를 입력
+
+
+                // [2-2]. parameter 전달 및 데이터 읽어오기.
+                String strParams = threadParam; //sbParams에 정리한 파라미터들을 스트링으로 저장. 예)id=id1&pw=123;
+                OutputStream os = urlConn.getOutputStream();
+                os.write(strParams.getBytes("UTF-8")); // 출력 스트림에 출력.
+                os.flush(); // 출력 스트림을 플러시(비운다)하고 버퍼링 된 모든 출력 바이트를 강제 실행.
+                os.close(); // 출력 스트림을 닫고 모든 시스템 자원을 해제.
+
+                // [2-3]. 연결 요청 확인.
+                // 실패 시 null을 리턴하고 메서드를 종료.
+                if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK) return;
+
+                // [2-4]. 읽어온 결과물 리턴.
+                // 요청한 URL의 출력물을 BufferedReader로 받는다.
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(
+                                urlConn.getInputStream(),
+                                "UTF-8"
+                        )
+                );
+
+                // 출력물의 라인과 그 합에 대한 변수.
+                String line;
+                String page = "";
+
+                // 라인을 받아와 합친다.
+                while ((line = reader.readLine()) != null){
+                    page += line;
+                }
+
+                Result = page;
+
+            } catch (MalformedURLException e) { // for URL.
+                e.printStackTrace();
+            } catch (IOException e) { // for openConnection().
+                e.printStackTrace();
+            } finally {
+            }
+        }
+        public String getResult() {
+            return this.Result;
+        }
+    }
+    public void setIdPw(@NonNull Hashtable<String, Hashtable<String, String>> htLogin, String club, String id, String pw) {
+        Hashtable<String, String> ht_island = new Hashtable<String, String>();
+        ht_island.put("id", id);
+        ht_island.put("pw", pw);
+        htLogin.put(club, ht_island);
+    }
+    public String setStringTemplate(@NonNull Hashtable<String, String> params, String template) {
+        for(String key:params.keySet()) {
+            String val = params.get(key);
+            // Log.d("param", key + "::" + val);
+            String regex = "\\$\\{" + key + "\\}";
+            template = template.replaceAll(regex, val);
+        }
+        // Log.d("template", template);
+        return template;
+    }
 }
